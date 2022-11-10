c74d151caf289c1493eb21f8bbf41d3342478100
Arshad Ullah <mdarshadullah835@gmail.com>
2022-11-10 21:52:33 +0600

    error page added

---
+++ src/Pages/ErrorPage/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className='text-center py-20'>
+            <h1 className='text-6xl text-semibold text-red-600'>Page is not found 404</h1>
+        </div>
+    );
+};
+
+export default Error;
